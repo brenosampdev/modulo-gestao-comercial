587147f13e68b9abd9218d9ab2eb6ec1d373032d
Breno Sampaio <brenosg2310@gmail.com>
2026-05-16 21:16:34 -0300

    fix(formatters): Ajust floating point case

--- src/shared/utils/formatters.ts
+++ src/shared/utils/formatters.ts
@@ -4,7 +4,7 @@ import type { IOrder, IOrderItem, IOrderPayment } from "../../types/IOrder";
 function formatDate(date: string): string {
     const normalizeDate = date.trim()
     
-    if (!isValid(parseISO(normalizeDate)) || normalizeDate.length < 19) {
+    if (!isValid(parseISO(normalizeDate)) || normalizeDate.length !== 19) {
         return 'Data inválida'
     }
     
@@ -13,18 +13,18 @@ function formatDate(date: string): string {
 
 function calcOrderTotal(items: IOrderItem[]): number {
     const total = items.reduce((totalAmount: number, item: IOrderItem) => {
-        return totalAmount + (item.quantity * item.unitPrice);
+        return totalAmount + item.quantity * item.unitPrice
     }, 0);
     
-    return total
+    return Math.round(total * 100) / 100;
 }
 
 function calcPaymentTotal(payment: IOrderPayment[]): number {
     const sumPayments = payment.reduce((total: number, amount: IOrderPayment) => {
-        return total + amount.amount
+        return (total + amount.amount)
     }, 0)
     
-    return sumPayments
+     return Math.round(sumPayments * 100) / 100
 }
 
 function formatCurrency(value: number): string {
@@ -35,18 +35,14 @@ function formatCurrency(value: number): string {
 }
 
 function calcInstallmentValue(total: number, installments: number): number {
-    if (installments < 1) {
-        return 1
-    }
-
     return Math.round((total / installments) * 100) / 100;
 }
 
 function isPaymentComplete(order: IOrder): boolean {
     const sumItems = calcOrderTotal(order.items)
     const sumPayments = calcPaymentTotal(order.payments)
-    
-    if (sumItems === sumPayments) {
+
+    if (sumItems == sumPayments) {
         return true
     }
 
@@ -56,6 +52,7 @@ function isPaymentComplete(order: IOrder): boolean {
 export {
     formatDate,
     calcOrderTotal,
+    calcPaymentTotal,
     formatCurrency,
     calcInstallmentValue,
     isPaymentComplete
