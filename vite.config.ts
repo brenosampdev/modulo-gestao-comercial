9a57e47c45a5d98106daf440313bdf2c4ddeb0e1
Breno Sampaio <brenosg2310@gmail.com>
2026-05-17 15:43:16 -0300

    refactor(config): configure path aliases and test

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import * as path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,15 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      }
+    ]
+  },
+  test: {
+    globals: true
+  },
 })
