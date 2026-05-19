aaa4cd719bf7500531d2de671b4bea18d28c1d2e
Breno Sampaio <brenosg2310@gmail.com>
2026-05-19 03:45:10 -0300

    feat(routes): add order and payment routes

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,6 +3,10 @@ import AppLayout from "@/shared/components/AppLayout"
 import { paths } from "@/config/paths"
 import ProductsListPage from "@/features/products/pages/ProductsListPage"
 import ProductsNewPage from "@/features/products/pages/ProductsNewPage"
+import PaymentMethodListPage from "@/features/payment-methods/pages/PaymentMethodListPage"
+import PaymentMethodNewPage from "@/features/payment-methods/pages/PaymentMethodNewPage"
+import OrdersListPage from "@/features/orders/pages/OrdersListPage"
+import OrdersNewPage from "@/features/orders/pages/OrdersNewPage"
 
 function AppRoutes() {
     return (
@@ -11,10 +15,10 @@ function AppRoutes() {
           <Route path={paths.home.path} element={<Navigate to={paths.orders.list.getHref()} replace/>}/>
           <Route path={paths.products.list.path} element={<ProductsListPage/>}/>
           <Route path={paths.products.new.path} element={<ProductsNewPage/>}/>
-          {/* <Route path={paths.paymentMethods.list.path} element={<App/>}/> */}
-          {/* <Route path={paths.paymentMethods.new.path} element={<App/>}/> */}
-          {/* <Route path={paths.orders.list.path} element={<AppLayout />}/> */}
-          {/* <Route path={paths.orders.new.path} element={<App/>}/> */}
+          <Route path={paths.paymentMethods.list.path} element={<PaymentMethodListPage/>}/>
+          <Route path={paths.paymentMethods.new.path} element={<PaymentMethodNewPage/>}/>
+          <Route path={paths.orders.list.path} element={<OrdersListPage/>}/>
+          <Route path={paths.orders.new.path} element={<OrdersNewPage/>}/>
         </Route>
       </Routes>
     )
